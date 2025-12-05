779f1f8b84fcf1a3ef136f929d9ecc40808b7ee4
AlfonsR <86108624+AlfonsR@users.noreply.github.com>
2025-12-06 00:40:11 +0700

    Add files via upload

--- lib/data.ts
+++ lib/data.ts
@@ -1,54 +1,154 @@
 export const data = [
   {
     id: "1",
-    title: "Roadtrip!",
+    title: "Roadtripppp!",
     // year: "17 February 2023",
     description:
       "Aaaaaaaaaa coo manyy roadtrippp cayanggg ummmmmm dan pastii akan adaa cocococoo many moreee 🤍🤍🤍🤍Ayokkk kitah bikinn cococooo manyy happy memoriess togetherr lewat roadtripp keyiyingg duniahh (kecuali India😬) campe matiii😘😘😘",
-    image: "/image/roadtripcover.jpeg",
+    image: '/image/roadtrip/1.jpeg',
     gallery: [
       {
         url: '/image/roadtrip/1.jpeg',
+        desc: 'Tempatnyahh ubuagucuuuu ummm mammnyahh jugakk enyakk tuuuu nyomnyoommmm'
+      },
+      {
+        url: '/image/roadtrip/2.jpeg',
+        desc: 'ummmm cukakkcukakkk discoverr new placessss'
+      },
+      {
+        url: '/image/roadtrip/3.jpeg',
+        desc: 'Aaaaaaa atutttt diaommmm tpi cayangmuu maoo aommm cayangg teyusss😘😘😘😘'
+      },
+      {
+        url: '/image/roadtrip/4.jpeg',
+        desc: 'ummmmm cukakkk doa bayenggg di GSV ayokk lagiiii'
+      },
+      {
+        url: '/image/roadtrip/5.jpeg',
+        desc: 'Cayangg nda takutt kann potoo dicanahh??👀👀'
+      },
+      {
+        url: '/image/roadtrip/6.jpeg',
+        desc: 'Uenyakuuuuu mammnyah tuuu upueddecuuu walaupun nunggu 2 jamm😬😬😬'
+      },
+      {
+        url: '/image/roadtrip/7.jpeg',
+        desc: 'AAAAA ceyuceyuceyuuuu tpi ada yg bokongnyahh ketatapp tuu ehehhe coyi cayanggg'
+      },
+      {
+        url: '/image/roadtrip/8.jpeg',
         desc: 'Aaaa canteraa baguss tempatnyahh teyuss ademmms tpi ada yg ketakutan naik ferris wheel tuuu👀👀'
       },
       {
-        url: '/image/roadtrip/1.jpeg',
-        desc: ''
-      },
-      {
-        url: '/image/roadtrip/1.jpeg',
-        desc: ''
-      },
-      {
-        url: '/image/roadtrip/1.jpeg',
-        desc: ''
+        url: '/image/roadtrip/9.jpeg',
+        desc: 'firstt roadtripppp🥰🥰🥰 capaa yaa yang ngerangkulnyahh macii kakuu tuu hehehehe'
       },
     ]
   },
   {
     id: "2",
-    title: "Photoshoot!",
+    title: "Photoshoooot!",
     // year: "4 March 2023",
     description:
       "Ududududuuu cayangkuu ceyayuu cantiqq upuoyuuu pass lagi nda ngapa ngapainn, apalagii pas lagi potociudd aaaaaaaaa meyeyehh cayangmuu tolonggg💦💦💦Maoo potociudd yang ubuanyakuu teyuss nantik potociudd nikahhh ummm teyusteyuss potociudd bayengg tututt titittt ummmm maomaomaoooo🥰🥰🥰🥰",
-    image: "/image/photoshootcover.jpeg",
+    image: '/image/photoshoot/1.jpeg',
     gallery: [
       {
         url: '/image/photoshoot/1.jpeg',
         desc: 'Our first ever photoshoott as a cutie koala coupleee ummmmm🤍🤍🤍'
       },
       {
-        url: '/image/photoshoot/1.jpeg',
-        desc: ''
+        url: '/image/photoshoot/2.jpeg',
+        desc: 'Ummm coo manyy byutipull potohhhh'
       },
       {
-        url: '/image/photoshoot/1.jpeg',
-        desc: ''
+        url: '/image/photoshoot/3.jpeg',
+        desc: 'uuuuuu cungguh ceyiussss👀👀ubuagucuuu yayyyy'
       },
       {
-        url: '/image/photoshoot/1.jpeg',
-        desc: ''
+        url: '/image/photoshoot/4.jpeg',
+        desc: 'Loploppppp ummm estetiqq upuoyuuu🥰🥰🥰'
       },
+      {
+        url: '/image/photoshoot/5.jpeg',
+        desc: 'Um um ummm maoo peyukkk gituhhh🥺🥺'
+      },
+      {
+        url: '/image/photoshoot/6.jpeg',
+        desc: 'ehhh hampir pegangg apatuhhh ehehhe empukkk upuoyuuu😘😘😘'
+      },
+      {
+        url: '/image/photoshoot/7.jpeg',
+        desc: 'Haloooo tuttttt aaaaaaa gemecinnnn umumumumuuu'
+      },
+      // {
+      //   url: '/image/photoshoot/8.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/9.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/10.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/11.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/12.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/13.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/14.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/15.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/16.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/17.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/18.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/19.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/20.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/21.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/22.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/23.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/photoshoot/24.jpeg',
+      //   desc: ''
+      // },
     ]
   },
   {
@@ -57,102 +157,276 @@ export const data = [
     // year: "",
     description:
       "Ummmm agak bittersweet yaa yang inii🥺walaupunn ci hitam baukk tuu dah bobokk bayeng Tuhan, tapi yang pentingg diaa dicini happy bayeng kitanyahh dan cayangmu bercyukur dan happyy upuoyuu bicah ngajak jalann bruccel bayeng cayangg🤍🤍🤍",
-    image: "/image/gugukwalkcover.jpeg",
+    image: '/image/guguk/1.jpeg',
     gallery: [
       {
         url: '/image/guguk/1.jpeg',
-        desc: 'Pose posee yang passs nii jadi cover albumm 🐾 ehehehe'
+        desc: 'Baaa haloo cellll happy upuoyuuu tuuuu ruccelll🥰🥰🥰🥰'
       },
       {
-        url: '/image/guguk/1.jpeg',
-        desc: ''
+        url: '/image/guguk/2.jpeg',
+        desc: 'ehehehhee terjebakk didayemm penjarahh aafff'
       },
       {
-        url: '/image/guguk/1.jpeg',
-        desc: ''
+        url: '/image/guguk/3.jpeg',
+        desc: 'ummmm maooo lagiiii:('
       },
       {
-        url: '/image/guguk/1.jpeg',
-        desc: ''
+        url: '/image/guguk/4.jpeg',
+        desc: 'nyomnyomnyomm brrrbrrrr ademmm'
       },
+      {
+        url: '/image/guguk/5.jpeg',
+        desc: 'ada yang teparr nampaknyahhh😬😬😬'
+      },
+      {
+        url: '/image/guguk/6.jpeg',
+        desc: 'ummmm hallo ruccellllll'
+      },
+      {
+        url: '/image/guguk/7.jpeg',
+        desc: 'pokuss upuoyuuu ke cemiyannyahhh hukkk👀👀👀'
+      },
+      // {
+      //   url: '/image/guguk/8.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/guguk/9.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/guguk/10.jpeg',
+      //   desc: ''
+      // },
     ]
   },
   {
     id: "4",
-    title: "Our Special Dinners",
+    title: "Foodie Taimmm",
     // year: "2021",
     description:
       "Maooo keyiyingg duniahh untukk coba cobainn cemuahh mammm yang enyakk upuoyuuu, apalagi tempat tempat dinnerr yang ubuagucuu umuewahuuuu cemoamoanyahh pokoknyah bayengg cayanggg🥰🥰🥰hippoo ciapp untukk menghicapp cemoamoaahh mammmm apalagii menghicapp cayanggg🥵🥵🥵🥵",
-    image: "/image/dinnercover.jpeg",
+    image: '/image/foodie/1.jpeg',
     gallery: [
       {
-        url: '/image/dinner/1.jpeg',
-        desc: 'Ummmmmm dinner teyakhirr cebeyummm cayangmu ditinggal ke Jakarta huhuhu😭😭😭'
+        url: '/image/foodie/1.jpeg',
+        desc: 'AAAAAA HENCINNN DINNERRRR🥰🥰🥰'
       },
       {
-        url: '/image/dinner/1.jpeg',
-        desc: ''
+        url: '/image/foodie/2.jpeg',
+        desc: 'umm kecukaann kitah cemuahh tuuuu nyomnyomm upuoyuuu'
       },
       {
-        url: '/image/dinner/1.jpeg',
-        desc: ''
+        url: '/image/foodie/3.jpeg',
+        desc: 'Bang aliiiii always debestttt🤍🤍'
       },
       {
-        url: '/image/dinner/1.jpeg',
-        desc: ''
+        url: '/image/foodie/4.jpeg',
+        desc: 'Uuuuuuu bebekk yang menghancurkan cemoahh bebek lainnyahhh'
       },
+      {
+        url: '/image/foodie/5.jpeg',
+        desc: 'Capanii yang jadi cukakk nambahinn maduu ke pizzahhhh???'
+      },
+      {
+        url: '/image/foodie/6.jpeg',
+        desc: 'P.A.G.I. C.O.Y.E.'
+      },
+      {
+        url: '/image/foodie/7.jpeg',
+        desc: 'Karee pikkk ummm maoomaomaoo lagiiii'
+      },
+      // {
+      //   url: '/image/foodie/8.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/foodie/9.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/foodie/10.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/foodie/11.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/foodie/12.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/foodie/13.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/foodie/14.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/foodie/15.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/foodie/16.jpeg',
+      //   desc: ''
+      // },
     ]
   },
+  // {
+  //   id: "5",
+  //   title: "My Princess Cookingg!",
+  //   // year: "2021",
+  //   description:
+  //     "Aaaaaaa coo manyyy macakannyahh cayanggg uenyakuu upuoyuuu cemoamoanyahhh ummmmm tuu pastii macii banyakk yang lomm cayangmu masukinn caking banyaknyahh yg cayang macakinnn untuk cayangmuuuu tpitpitpii maooo dimacakinn teyuss camah cayang campe matiiii😘😘😘😘😘😘",
+  //   image: '/image/cooking/1.jpeg',
+  //   gallery: [
+  //     {
+  //       url: '/image/cooking/1.jpeg',
+  //       desc: 'Aaaa naci hoheng kimchiii enyak upuoyuu ummmmm'
+  //     },
+  //     {
+  //       url: '/image/cooking/2.jpeg',
+  //       desc: ''
+  //     },
+  //     {
+  //       url: '/image/cooking/3.jpeg',
+  //       desc: ''
+  //     },
+  //     {
+  //       url: '/image/cooking/4.jpeg',
+  //       desc: ''
+  //     },
+  //     {
+  //       url: '/image/cooking/5.jpeg',
+  //       desc: ''
+  //     },
+  //     {
+  //       url: '/image/cooking/6.jpeg',
+  //       desc: ''
+  //     },
+  //   ]
+  // },
   {
-    id: "5",
+    id: "6",
     title: "JKT Taimmm",
     // year: "2021",
     description:
       "Ummmmm alwayss happy happyyy upuoyuu bicahh ngelepass kangenn bayengg cayangg apalagii cebebass ituhh teyuss bicahh keyonann noncetopp bicah bobok bayengg jalan jalann bayengg campe cubuhh bicahh ah ah aahh- ehhh makcudnyahh ketawa bayeng teyusss ehehehe😘😘😘ummmm maoo ceped cepedd nyucull cayang ke Jakartaa cecepednyahh biar nda perlu nunggu waktuu untuk ketemooo tpi bicah langcungg cetiap hayii cetiapp caattt ummmm aminnnnnn🤍🤍🤍🤍",
-    image: "/image/jkttaimcover.jpeg",
+    image: '/image/jkt/1.jpeg',
     gallery: [
       {
         url: '/image/jkt/1.jpeg',
         desc: 'Aaaaaa pertama kalii datengin cayangg ke Jakartaa ummm ada yang campe nyiapinn WELCOME tuuu ummm imudnyahh cayangkuu niii🥰🥰🥰'
       },
       {
-        url: '/image/jkt/1.jpeg',
-        desc: ''
+        url: '/image/jkt/2.jpeg',
+        desc: 'Ehehehe mickeyy minnieee'
       },
       {
-        url: '/image/jkt/1.jpeg',
-        desc: ''
+        url: '/image/jkt/3.jpeg',
+        desc: 'CAPYYYYYYYYYYYYYYYYY'
       },
       {
-        url: '/image/jkt/1.jpeg',
-        desc: ''
+        url: '/image/jkt/4.jpeg',
+        desc: 'Ummmmm ubuagucuu tempatnyahh imudimuddd cemoamoanyahh🥰🥰🥰'
       },
+      {
+        url: '/image/jkt/5.JPG',
+        desc: 'Um um ummm PIKKKK maoo ke PIKK lagiiiiii'
+      },
+      {
+        url: '/image/jkt/6.jpeg',
+        desc: 'DUFAN TAIMMMM tpitpii ciapahh yaa yang biyangg nda ucah pakekk jas hujannyahhh ehehehhee😘😘😘'
+      },
+      {
+        url: '/image/jkt/7.jpeg',
+        desc: 'Uuuuuu cekcinyahhh cayangkuuuuu ah ah ahhh tpi ada yg bodoo pas puyangg huhhh ndaa akan terulang lagiii😤😤😤😤'
+      },
+      {
+        url: '/image/jkt/8.jpeg',
+        desc: 'Ummmm dinnerrr teyusteyuuss potonyahh ubuagucuuu cayangkuu cantiqq upuoyuuu ummmm🥰🥰🥰🥰'
+      },
+      // {
+      //   url: '/image/jkt/9.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/jkt/10.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/jkt/11.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/jkt/12.jpeg',
+      //   desc: ''
+      // },
     ]
   },
   {
-    id: "6",
-    title: "Random Things",
+    id: "7",
+    title: "Random Things!",
     // year: "2021",
     description:
       "Ehehehehhehe lots of collectionn kerandomann kitahhh dan jugakk keimudann cayangkuu yang tanpa batasss niii yang HUUDUDUDUUDUDUUDU UMUMUMMUUMMUMUUU IMUDNYAHHHH🥰🥰🥰🥰cukakcukakcukakkkkk my cutie koayaa uimuduuuu maoo cayangmu nyemek nyemekkkk noncetopppppppp- eh👀",
-    image: "/image/randomcover.jpeg",
+    image: '/image/random/1.jpeg',
     gallery: [
       {
         url: '/image/random/1.jpeg',
-        desc: 'Berpayungan (dan maci bacahh😬) untuk ngambil duidd bayar artomoyoo nii nampaknyahh👀👀'
+        desc: 'Ehehehhee cayangkuu ndaa mayuuu kannn tuu upuedeuuu kannn😘😘😘😘'
       },
       {
-        url: '/image/random/1.jpeg',
-        desc: ''
+        url: '/image/random/2.jpeg',
+        desc: 'Cukupp miyipp kann cayangggg'
       },
       {
-        url: '/image/random/1.jpeg',
-        desc: ''
+        url: '/image/random/3.jpeg',
+        desc: 'Ududuuduu ada yang lemess tuu abiss ngapainn yahh dii dayemm aafff👀👀👀'
       },
       {
-        url: '/image/random/1.jpeg',
-        desc: ''
+        url: '/image/random/4.jpeg',
+        desc: 'Baaa cayangkuu bertemann camah cicakk yayyyy'
       },
+      {
+        url: '/image/random/5.jpeg',
+        desc: 'Ummmm aminnnnnnn cecepednyahh punyah lekcussss🤍🤍🤍'
+      },
+      {
+        url: '/image/random/6.jpeg',
+        desc: 'Ada yangg nyolonnggg tuu kayaknyahhh👀👀'
+      },
+      {
+        url: '/image/random/7.jpeg',
+        desc: 'Berpayungan (dan maci kehujanann😬) untuk ngambil duidd bayar artomoyoo nii nampaknyahh👀👀'
+      },
+      {
+        url: '/image/random/8.jpeg',
+        desc: 'Tututtt men tutt gugukkk ehehhe'
+      },
+      {
+        url: '/image/random/9.jpeg',
+        desc: 'Aaaaaa ceyuu ujan ujannn bayeng cayangg apalagii abiss dupan taimm ummmmm'
+      },
+      {
+        url: '/image/random/10.jpeg',
+        desc: 'Detektipp modeee fully onnnn👀👀'
+      },
+      // {
+      //   url: '/image/random/11.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/random/12.jpeg',
+      //   desc: ''
+      // },
+      // {
+      //   url: '/image/random/13.jpeg',
+      //   desc: ''
+      // },
     ]
   },
 ]
